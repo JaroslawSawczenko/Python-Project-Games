29b00902218c0cd14f72bc86a4ccdf2d03147a45
Liudmyla <105510660+light-twighlight@users.noreply.github.com>
2025-06-17 18:08:00 +0200

    read and write to json file

--- core/user.py
+++ core/user.py
@@ -1,5 +1,14 @@
 import json
 
+def read_data_user_json():
+    with open("../data/user_data.json", "r") as json_file:
+        data = json.load(json_file)
+    return data
+
+def write_data_user_json(data):
+    with open("../data/user_data.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
 class User:
     def __init__(self):
         self.user_info = {
@@ -8,23 +17,34 @@ class User:
             "results": {},
         }
 
+    # Stworzenie użytkownika. Potrzebuje tylko imię.
+
     def create_user(self, name):
-        with open("../data/user_data.json", "r") as json_file:
-            data = json.load(json_file)
+        data = read_data_user_json() # czytamy plik
 
         self.user_info["id"] = len(data)
         self.user_info["name"] = name
         data.append(self.user_info)
 
-        with open("../data/user_data.json", "w") as json_file:
-            json.dump(data, json_file, indent=4)
+        write_data_user_json(data) # zapis pliku
 
+    # Dodawanie wyników. Potrzebuje nazwę gry i wynik.
+
+    def add_results(self, game_name, score):
+        self.user_info["results"][game_name] = score
+        data = read_data_user_json() # czytamy plik
+
+        data[self.user_info["id"]]["results"] = self.user_info["results"]
+
+        write_data_user_json(data) # zapis pliku
 
 
 if __name__ == "__main__":
 
     user1 = User()
     user1.create_user("Test")
+
+    user1.add_results("sudoku", 100)
 
 
     # data = {
@@ -41,7 +61,6 @@ if __name__ == "__main__":
     # with open("user_data.json", "w") as json_file:
     #     json.dump(data_read, json_file, indent=4)
     #
-    with open("../data/user_data.json", "r") as json_file:
-        data_read = json.load(json_file)
+    data_read = read_data_user_json() # czytamy plik
 
     print(data_read)
